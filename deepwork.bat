3cb704936b3cd7854f704e8de4a5caa816f4dd11
BurnyCoder <happymancz@email.cz>
2025-12-18 21:20:04 +0100

    Increase browser delay to 3s

--- deepwork.bat
+++ deepwork.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d %~dp0
 powershell -Command "Start-Process python -ArgumentList 'frontend.py' -Verb RunAs -WorkingDirectory '%~dp0'"
-timeout /t 2 /nobreak >nul
+timeout /t 3 /nobreak >nul
 start http://localhost:5000
